c28a4886ec151a8d94361d590002a81a6c8cad06
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2023-06-17 05:53:36 +0200

    Updated package checks for Spack integration.

--- cmake/find_dependencies.cmake
+++ cmake/find_dependencies.cmake
@@ -1,38 +1,117 @@
 
-# Check for MPI installation.
-find_package(MPI COMPONENTS C CXX REQUIRED)
+# Search for MPI installation.
+find_package(MPI REQUIRED)
 
-# Check for zlib installation
-find_package(ZLIB REQUIRED)
-
-# Check for Python3
-find_package(Python3 COMPONENTS Interpreter Development NumPy REQUIRED)
-
-# Check for Eigen3
-find_package(Eigen3 REQUIRED NO_MODULE)
-if(${Eigen3_FOUND})
-  message(STATUS "Found Eigen3: yes")
+# Search for Vc installation.
+find_package(Vc REQUIRED)
+if(${Vc_FOUND})
+ message(STATUS "Found Vc: ${Vc_DIR} (found version ${Vc_VERSION})")
 endif()
 
-# Check for libboost
+# Search for zlib installation.
+find_package(ZLIB REQUIRED)
+
+# Search for python3 installation.
+find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
+
+# Search for Eigen installation.
+find_package(Eigen3 REQUIRED NO_MODULE)
+if(${Eigen3_FOUND})
+ message(STATUS "Found Eigen version ${Eigen3_VERSION} at ${Eigen3_DIR}")
+endif()
+
+# Search for Boost installation.
 find_package(Boost REQUIRED COMPONENTS log log_setup thread system filesystem program_options unit_test_framework)
 
-# Check for libxml2
+# Search for xml2 installation.
 find_package(LibXml2 REQUIRED)
 
-# Check for easylogging++
-include(cmake/FindEASYLOGGINGPP.cmake)
+# Search for preCICE installation.
+find_package(precice REQUIRED)
+if(${precice_FOUND})
+ message(STATUS "Found preCICE version ${precice_VERSION} at ${precice_DIR}")
+endif()
 
-# Check for base64
-# NOTE: This is not the same as GNU base64. Find the source code at https://github.com/tkislan/base64.
-include(cmake/FindBASE64.cmake)
+# Search for googletest installation.
+find_package(GTest REQUIRED)
 
-# Check for Vc
-find_package(Vc REQUIRED)
+# Search for ADIOS2 installation.
+find_package(adios2 REQUIRED)
+
+# Search for Easylogging++ installation.
+pkg_check_modules(EASYLOGGINGPP REQUIRED easyloggingpp)
+if(${EASYLOGGINGPP_FOUND})
+ message(STATUS "Found easyloggingpp at ${EASYLOGGINGPP_PREFIX}")
+ add_library(elpp SHARED ${EASYLOGGINGPP_INCLUDE_DIRS}/easylogging++.h ${EASYLOGGINGPP_INCLUDE_DIRS}/easylogging++.cc)
+ set(EASYLOGGINGPP_LIBRARIES elpp)
+endif()
+
+# Search for PETSc installation.
+pkg_check_modules(PETSC REQUIRED PETSc)
+if(${PETSC_FOUND})
+ message(STATUS "Found PETSc at ${PETSC_PREFIX}")
+endif()
+
+# Search for xbraid installation.
+# The library does not generate *.pc or *-config.cmake files.
+# The includes and libs are pointed directly.
+set(XBRAID_LIBRARIES "${xbraid_DIR}/lib")
+set(XBRAID_INCLUDE_DIR "${xbraid_DIR}/include")
+
+# Search for Base64 installation.
+# The library has no support for CMake or Autotools and it does not generate any static/shared libs.
+# A shared library is generated manually.
+find_path(BASE64_INCLUDE_DIR NAMES base64.h HINTS ${base64_DIR})
+if(EXISTS ${BASE64_INCLUDE_DIR})
+ message(STATUS "Found base64: ${base64_DIR}")
+endif()
+add_library(base64 SHARED ${BASE64_INCLUDE_DIR}/base64.h)
+set_target_properties(base64 PROPERTIES LINKER_LANGUAGE CXX)
+set(BASE64_LIBRARIES "base64")
+
+# Search for SEMT installation.
+# The library has no support for CMake or Autotools and it does not generate any static/shared libs.
+# A shared library is generated manually.
+find_path(SEMT_INCLUDE_DIR NAMES Semt.h HINTS "${semt_DIR}/semt")
+if(EXISTS ${SEMT_INCLUDE_DIR})
+ message(STATUS "Found SEMT: ${semt_DIR}")
+endif()
+add_library(semt SHARED
+            ${semt_DIR}/semt/BinaryOperators.h
+            ${semt_DIR}/semt/BinaryTypes.h
+            ${semt_DIR}/semt/Common.h
+            ${semt_DIR}/semt/Conditions.h
+            ${semt_DIR}/semt/Constexpr.h
+            ${semt_DIR}/semt/DifferentiableVectorExpr.h
+            ${semt_DIR}/semt/Expression.h
+            ${semt_DIR}/semt/Forwards.h
+            ${semt_DIR}/semt/Macros.h
+            ${semt_DIR}/semt/Parameter.h
+            ${semt_DIR}/semt/Semtfwd.h
+            ${semt_DIR}/semt/Semt.h
+            ${semt_DIR}/semt/Sequence.h
+            ${semt_DIR}/semt/Shortcuts.h
+            ${semt_DIR}/semt/Simplifications.h
+            ${semt_DIR}/semt/Traits.h
+            ${semt_DIR}/semt/UnaryOperator.h
+            ${semt_DIR}/semt/UnaryTypes.h
+            ${semt_DIR}/semt/Variable.h
+            ${semt_DIR}/semt/VectorExpr.cpp
+            ${semt_DIR}/semt/VectorExpr.h
+            ${semt_DIR}/loki/EmptyType.h
+            ${semt_DIR}/loki/NullType.h
+            ${semt_DIR}/loki/static_check.h
+            ${semt_DIR}/loki/Typelist.h
+            ${semt_DIR}/loki/TypelistMacros.h
+            ${semt_DIR}/loki/TypeManip.h
+          )
+set(SEMT_LIBRARIES "semt")
 
 include_directories(${MPI_CXX_INCLUDE_DIRS}
                     ${Python3_INCLUDE_DIRS}
                     ${LIBXML2_INCLUDE_DIR}
                     ${ZLIB_INCLUDE_DIRS}
                     ${Vc_INCLUDE_DIR}
+                    ${PETSC_INCLUDE_DIRS}
+                    ${EASYLOGGINGPP_INCLUDE_DIRS}
                    )
