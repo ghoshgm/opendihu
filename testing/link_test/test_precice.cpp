e62673f3767a89d554e90d69ab5b1921a3db6e3c
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2023-06-18 13:37:46 +0200

    Added link test for preCICE.

---
+++ testing/link_test/test_precice.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <SolverInterface.hpp>
+
+int main(int argc, char** argv)
+{
+  std::cout << precice::getVersionInformation() << std::endl;
+  
+  return EXIT_SUCCESS;
+}
