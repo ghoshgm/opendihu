0a3a8d3e85da28da5770397d87a30011e6824445
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2023-06-25 14:38:40 +0200

    Turn on semt, base64, gtest checks again.

--- cmake/find_dependencies.cmake
+++ cmake/find_dependencies.cmake
@@ -38,7 +38,7 @@ if(${precice_FOUND})
 endif()
 
 # Search for googletest installation.
-#find_package(GTest REQUIRED)
+find_package(GTest REQUIRED)
 
 # Search for ADIOS2 installation.
 #find_package(adios2 REQUIRED)
@@ -90,57 +90,58 @@ endif()
 # Search for Base64 installation.
 # The library has no support for CMake or Autotools and it does not generate any static/shared libs.
 # A shared library is generated manually.
-#find_path(BASE64_INCLUDE_DIR NAMES base64.h HINTS ${base64_DIR})
-#if(EXISTS ${BASE64_INCLUDE_DIR})
-#  message(STATUS "Found base64: ${base64_DIR}")
-#  add_library(base64 SHARED ${BASE64_INCLUDE_DIR}/base64.h)
-#  set_target_properties(base64 PROPERTIES LINKER_LANGUAGE CXX)
-#  set(BASE64_LIBRARIES "base64")
-#else()
-#  message(FATAL_ERROR "CMake could not find base64.h in ${base64_DIR}.
-#                       Please make sure the path to the source code is correct.")
-#endif()
+find_path(BASE64_INCLUDE_DIR NAMES base64.h HINTS ${base64_DIR})
+if(EXISTS ${BASE64_INCLUDE_DIR})
+  message(STATUS "Found base64: ${base64_DIR}")
+  add_library(base64 SHARED ${BASE64_INCLUDE_DIR}/base64.h)
+  set_target_properties(base64 PROPERTIES LINKER_LANGUAGE CXX)
+  set(BASE64_LIBRARIES "base64")
+else()
+  message(FATAL_ERROR "CMake could not find base64.h in ${base64_DIR}.
+                       Please make sure the path to the source code is correct.")
+endif()
 
 # Search for SEMT installation.
 # The library has no support for CMake or Autotools and it does not generate any static/shared libs.
 # A shared library is generated manually.
-#find_path(SEMT_INCLUDE_DIR NAMES Semt.h HINTS ${semt_DIR}/semt)
-#if(EXISTS ${SEMT_INCLUDE_DIR})
-#  message(STATUS "Found SEMT: ${semt_DIR}")
-#  add_library(semt SHARED
-#            ${semt_DIR}/semt/BinaryOperators.h
-#            ${semt_DIR}/semt/BinaryTypes.h
-#            ${semt_DIR}/semt/Common.h
-#            ${semt_DIR}/semt/Conditions.h
-#            ${semt_DIR}/semt/Constexpr.h
-#            ${semt_DIR}/semt/DifferentiableVectorExpr.h
-#            ${semt_DIR}/semt/Expression.h
-#            ${semt_DIR}/semt/Forwards.h
-#            ${semt_DIR}/semt/Macros.h
-#            ${semt_DIR}/semt/Parameter.h
-#            ${semt_DIR}/semt/Semtfwd.h
-#            ${semt_DIR}/semt/Semt.h
-#            ${semt_DIR}/semt/Sequence.h
-#            ${semt_DIR}/semt/Shortcuts.h
-#            ${semt_DIR}/semt/Simplifications.h
-#            ${semt_DIR}/semt/Traits.h
-#            ${semt_DIR}/semt/UnaryOperator.h
-#            ${semt_DIR}/semt/UnaryTypes.h
-#            ${semt_DIR}/semt/Variable.h
-#            ${semt_DIR}/semt/VectorExpr.cpp
-#            ${semt_DIR}/semt/VectorExpr.h
-#            ${semt_DIR}/loki/EmptyType.h
-#           ${semt_DIR}/loki/NullType.h
-#            ${semt_DIR}/loki/static_check.h
-#            ${semt_DIR}/loki/Typelist.h
-#            ${semt_DIR}/loki/TypelistMacros.h
-#            ${semt_DIR}/loki/TypeManip.h
-#          )
-#  set(SEMT_LIBRARIES "semt")
-#else()
-#  message(FATAL_ERROR "CMake could not find Semt.h in ${semt_DIR}.
-#                       Please make sure the path to the source code is correct.")
-#endif()
+find_path(SEMT_INCLUDE_DIR NAMES Semt.h HINTS ${semt_DIR}/semt)
+if(EXISTS ${SEMT_INCLUDE_DIR})
+  message(STATUS "Found SEMT: ${semt_DIR}")
+  add_library(semt SHARED
+            ${semt_DIR}/semt/BinaryOperators.h
+            ${semt_DIR}/semt/BinaryTypes.h
+            ${semt_DIR}/semt/Common.h
+            ${semt_DIR}/semt/Conditions.h
+            ${semt_DIR}/semt/Constexpr.h
+            ${semt_DIR}/semt/DifferentiableVectorExpr.h
+            ${semt_DIR}/semt/Expression.h
+            ${semt_DIR}/semt/Forwards.h
+            ${semt_DIR}/semt/Macros.h
+            ${semt_DIR}/semt/Parameter.h
+            ${semt_DIR}/semt/Semtfwd.h
+            ${semt_DIR}/semt/Semt.h
+            ${semt_DIR}/semt/Sequence.h
+            ${semt_DIR}/semt/Shortcuts.h
+            ${semt_DIR}/semt/Simplifications.h
+            ${semt_DIR}/semt/Traits.h
+            ${semt_DIR}/semt/UnaryOperator.h
+            ${semt_DIR}/semt/UnaryTypes.h
+            ${semt_DIR}/semt/Variable.h
+            ${semt_DIR}/semt/VectorExpr.cpp
+            ${semt_DIR}/semt/VectorExpr.h
+            ${semt_DIR}/loki/EmptyType.h
+           ${semt_DIR}/loki/NullType.h
+            ${semt_DIR}/loki/static_check.h
+            ${semt_DIR}/loki/Typelist.h
+            ${semt_DIR}/loki/TypelistMacros.h
+            ${semt_DIR}/loki/TypeManip.h
+          )
+  target_include_directories(semt PUBLIC ${semt_DIR})
+  set(SEMT_LIBRARIES "semt")
+else()
+  message(FATAL_ERROR "CMake could not find Semt.h in ${semt_DIR}.
+                       Please make sure the path to the source code is correct.")
+endif()
 
 include_directories(${MPI_CXX_INCLUDE_DIRS}
                     ${Python3_INCLUDE_DIRS}
